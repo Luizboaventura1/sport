fd88fec1cf3cebe8f83cd85ecf5b698899aaf35d
LuizHenrique1 <luizboaventura24@gmail.com>
2023-01-22 10:51:12 -0300

    Sport

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+$(function(){
+
+    $('.menu_burguer').click(function(){
+        let menu = $('#menu_open')
+        let close = $('#menu_close')
+
+        $('#menu_mobile').slideToggle()
+
+        if(menu.css('display') == 'block') {
+            menu.fadeOut().css('display','none')
+            close.fadeIn().css('display','block')
+        }else {
+            menu.fadeIn().css('display','block')
+            close.fadeOut().css('display','none')
+        }
+    })
+})
