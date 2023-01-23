2874c539d9668ed32decc05964ff33226c87b9b6
LuizHenrique1 <luizboaventura24@gmail.com>
2023-01-23 10:29:07 -0300

    Atualizado

--- JS/produtos.js
+++ JS/produtos.js
@@ -34,6 +34,23 @@ lojaOn = () => {
     produtosLoja.map((value)=> {
         produtosMostrar.append(`
         <div class="container_produto_single" id="${value.id}">
+            <img src="images/${value.img}" alt="Img produto">
+            <h1>${value.nome}</h1>
+            <p>R$ ${value.preco.toFixed(2).replace('.',',')}</p>
+            <div class="avaliacao_produto">
+                <img src="images/img-5-estrelas.png" alt="Img estrela">
+            </div>
+        </div>`)
+    })
+}
+
+lojaOn()
+
+lojaOn2 = () => {
+    let produtosMostrar = $('.produtos_loja')
+    produtosLoja.map((value)=> {
+        produtosMostrar.append(`
+        <div class="container_produto_single" id="${value.id}">
             <img src="../images/${value.img}" alt="Img produto">
             <h1>${value.nome}</h1>
             <p>R$ ${value.preco.toFixed(2).replace('.',',')}</p>
@@ -44,7 +61,7 @@ lojaOn = () => {
     })
 }
 
-lojaOn()
+lojaOn2()
 
 
 $('.promocao_wraper').slick({
